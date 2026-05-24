97112f2e3d790e1a7f02c67e2a66ed0468de507f
salomon edem djokoto <djokotosalomon@gmail.com>
2026-05-24 18:44:45 +0100

    Add Content Security Policy to allow unsafe-eval

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: *.amazonaws.com placehold.co via.placeholder.com; connect-src 'self' https://glabackend-production.up.railway.app http://localhost:5005;",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "via.placeholder.com" },
